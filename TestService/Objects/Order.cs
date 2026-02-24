7dc12c6811cc7518feffff1e825368be3d3f0a47
m.anderson <matt.anderson@fleetcor.com>
2026-02-24 12:07:14 +0000

    remove Test Service

--- TestService/Objects/Order.cs
+++
@@ -1,7 +0,0 @@
-public class Order
-{
-    public int Id { get; set; }
-    public string ProductName { get; set; } = string.Empty;
-    public int Quantity { get; set; }
-    public decimal Price { get; set; }
-}
--- TestService/api/Orders/CreateOrderRequest.cs
+++
@@ -1,6 +0,0 @@
-public class CreateOrderRequest
-{
-    public string ProductName { get; set; } = string.Empty;
-    public int Quantity { get; set; }
-    public decimal Price { get; set; }
-}
